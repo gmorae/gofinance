8f48a20ff0f67ff8f57e15e517608c32a45c60be
gmorae <gmmartins06@gmail.com>
2022-04-08 00:57:26 -0300

    create header and add colors and fonts in project

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://github.com/gmorae.png' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Gabriel</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  )
+}
